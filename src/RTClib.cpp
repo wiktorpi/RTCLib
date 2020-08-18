62e246e01209656409b46199ce96eda97e3b88f9
MinusWall <rratajs@gmail.com>
2020-08-18 22:00:27 +0200

    Update RTClib.cpp
    typos

--- src/RTClib.cpp
+++ src/RTClib.cpp
@@ -118,8 +118,8 @@ DateTime::DateTime(const __FlashStringHelper* date, const __FlashStringHelper* t
     char _time[8];
     memcpy_P(_date, date, 11);
     memcpy_P(_time, time, 8);
-    SetDate(date);
-    SetTime(time);
+    SetDate(_date);
+    SetTime(_time);
 }
 
 DateTime::DateTime(const char* sdate) : DateTime(sdate, sdate + 11) {}
